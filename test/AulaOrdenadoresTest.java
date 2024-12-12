155b3c964668e849c498b6e3dd39014f25d321c7
ManuelMolino <mmolmil158@g.educaand.es>
2024-12-12 14:03:05 +0100

    Siguiendo con test con JUnit

--- test/AulaOrdenadoresTest.java
+++ test/AulaOrdenadoresTest.java
@@ -8,6 +8,12 @@ class AulaOrdenadoresTest {
     @BeforeEach
     void setUp() {
         aula = new AulaOrdenadores("1.7");
+        Ordenador ordenador1 = new Ordenador(16, "axus", false);
+        aula.addOrdenador(ordenador1);
+        Ordenador ordenador2 = new Ordenador(16, "axus", true);
+        aula.addOrdenador(ordenador2);
+        Ordenador ordenador3 = new Ordenador(32, "msi", false);
+        aula.addOrdenador(ordenador3);
     }
 
     @Test
@@ -19,26 +25,33 @@ class AulaOrdenadoresTest {
     void setNombreAlula() {
         aula.setNombreAlula("1.8");
         assertEquals("1.8", aula.getNombreAlula());
-
     }
 
     @Test
     void getOrdenadores() {
-        assertEquals(0, aula.getOrdenadores().size());
+        assertEquals(3, aula.getOrdenadores().size());
     }
 
     @Test
     void addOrdenador() {
-        Ordenador ordenador1 = new Ordenador(16, "axus", false);
-        aula.addOrdenador(ordenador1);
-        assertEquals(1, aula.getOrdenadores().size());
-        Ordenador ordenador2 = new Ordenador(16, "msi", true);
-        aula.addOrdenador(ordenador2);
-        assertEquals(2, aula.getOrdenadores().size());
-        //assertEquals(ordenador1, ordenador2);
+        Ordenador ordenador = new Ordenador(8, "axus", true);
+        aula.addOrdenador(ordenador);
+        assertEquals(4, aula.getOrdenadores().size());
+        assertEquals(8, aula.getOrdenadores().get(4).ram());
+        assertEquals("axus", aula.getOrdenadores().get(4).placaBase());
+        assertTrue(aula.getOrdenadores().get(4).graficaIntegrada());
+        assertNull(aula.getOrdenadores().get(5));
     }
 
     @Test
     void eliminarOrdenador() {
+        Ordenador ordenador = aula.eliminarOrdenador(1);
+        assertEquals(2, aula.getOrdenadores().size());
+        assertEquals(16, ordenador.ram());
+        assertEquals("axus", ordenador.placaBase());
+        assertFalse(ordenador.graficaIntegrada());
+        ordenador = aula.eliminarOrdenador(1);
+        assertEquals(2, aula.getOrdenadores().size());
+        assertNull(ordenador);
     }
 }
